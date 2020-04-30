d6d14ed68d2dec647b7a2b6e48cd7627f4c71f6c
Zach Wahrer <zachtheclimber@gmail.com>
2020-04-30 16:17:09 -0600

    Add pagination via cursor.skip() in get_movies()

--- M220P/mflix-python/mflix/db.py
+++ M220P/mflix-python/mflix/db.py
@@ -227,8 +227,7 @@ def get_movies(filters, page, movies_per_page):
     the Pymongo cursor.
     """
 
-    # TODO: Paging
-    # Use the cursor to only return the movies that belong on the current page.
+    cursor.skip(page * movies_per_page)
     movies = cursor.limit(movies_per_page)
 
     return (list(movies), total_num_movies)
