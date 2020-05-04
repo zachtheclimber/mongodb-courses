27e56302056538f94ac856ea9f223d188547d279
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-04 10:16:08 -0600

    Add comment adding and updating functionality to db.py

--- M220P/mflix-python/mflix/db.py
+++ M220P/mflix-python/mflix/db.py
@@ -327,9 +327,11 @@ def add_comment(movie_id, user, comment, date):
 
     Name and email must be retrieved from the "user" object.
     """
-    # TODO: Create/Update Comments
-    # Construct the comment document to be inserted into MongoDB.
-    comment_doc = {"some_field": "some_value"}
+    comment_doc = {"name": user.name,
+                   "email": user.email,
+                   "movie_id": ObjectId(movie_id),
+                   "text": comment,
+                   "date": date}
     return db.comments.insert_one(comment_doc)
 
 
@@ -339,12 +341,9 @@ def update_comment(comment_id, user_email, text, date):
     based by both comment _id field as well as the email field to doubly ensure
     the user has permission to edit this comment.
     """
-    # TODO: Create/Update Comments
-    # Use the user_email and comment_id to select the proper comment, then
-    # update the "text" and "date" of the selected comment.
     response = db.comments.update_one(
-        {"some_field": "some_value"},
-        {"$set": {"some_other_field": "some_other_value"}}
+        {"_id": ObjectId(comment_id), "email": user_email},
+        {"$set": {"text": text, "date": date}}
     )
 
     return response
